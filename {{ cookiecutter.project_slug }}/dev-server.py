85afbba1f902fcbc1a8ea6e41ee78cf25a859889
tiago.paranhos <tiagoprn@gmail.com>
2020-04-29 18:37:18 -0300

    bugfix

--- {{ cookiecutter.project_slug }}/dev-server.py
+++ {{ cookiecutter.project_slug }}/dev-server.py
@@ -2,5 +2,5 @@ import {{ cookiecutter.project_slug }}
 from {{ cookiecutter.project_slug }} import factory
 
 if __name__ == "__main__":
-    app = factory.create_app(celery=app.celery)
+    app = factory.create_app(celery={{ cookiecutter.project_slug }}.celery)
     app.run()
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/factory.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/factory.py
@@ -1,9 +1,9 @@
 import os
 
 from celery import Celery
-from flask import Flask
 
-from {{ cookiecutter.project_slug }}.celery_utils import init_celery
+from flask import Flask
+from {{cookiecutter.project_slug}}.celery_utils import init_celery
 
 PKG_NAME = os.path.dirname(os.path.realpath(__file__)).split("/")[-1]
 
@@ -12,7 +12,7 @@ def create_app(app_name=PKG_NAME, **kwargs):
     app = Flask(app_name)
     if kwargs.get("celery"):
         init_celery(kwargs.get("celery"), app)
-    from app.api import blueprint as api_blueprint
+    from {{ cookiecutter.project_slug }}.api import blueprint as api_blueprint
 
     app.register_blueprint(api_blueprint)
     return app
